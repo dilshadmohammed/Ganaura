1838ba20c2723a9e3146d05a73592c2fee5fcdb4
Vedasree M <vedasree1811@gmail.com>
2025-02-20 23:39:02 +0530

    login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; // Import the Home component
 import Login from './pages/Login/Login';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import SignUp from './pages/SignUp/SignUp';
@@ -8,10 +9,13 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        {/* Default route (Home Page) */}
+        <Route path="/" element={<Home />} />
+
+        {/* Other routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Login />} /> {/* Default route */}
       </Routes>
     </Router>
   );
